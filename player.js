05adc6a78b18091e3ff05f4e3c599018b1762ba4
Krishna Bharadwaj <bharadwaj6598@gmail.com>
2017-08-25 23:32:22 +0530

    Done coding now testing

---
+++ player.js
@@ -0,0 +1,16 @@
+function playM3u8(url){
+  if(Hls.isSupported()) {
+      var video = document.getElementById('video');
+      video.volume = 0.3;
+      var hls = new Hls();
+      var m3u8Url = decodeURIComponent(url)
+      hls.loadSource(m3u8Url);
+      hls.attachMedia(video);
+      hls.on(Hls.Events.MANIFEST_PARSED,function() {
+        video.play();
+      });
+      document.title = url
+    }
+}
+
+playM3u8(window.location.href.split("#")[1])
