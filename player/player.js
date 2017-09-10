2f49f082176deec9193b297f8ab0b2f1b7d89949
Krishna Bharadwaj <bharadwaj6598@gmail.com>
2017-09-10 11:04:11 +0530

    Keyboard shortcuts working now

--- player/player.js
+++ player/player.js
@@ -1,8 +1,7 @@
-var videoObj = $('#video');
+var video = document.getElementById('video');
 
 function playM3u8(url){
   if(Hls.isSupported()) {
-      var video = document.getElementById('video');
       video.volume = 0.3;
       var hls = new Hls();
       var m3u8Url = decodeURIComponent(url)
@@ -16,19 +15,19 @@ function playM3u8(url){
 }
 
 function playPause() {
-    videoObj.paused?videoObj.play():videoObj.pause();
+    video.paused?video.play():video.pause();
 }
 
 function volumeUp() {
-    videoObj.volume+=0.1;
+    video.volume+=0.1;
 }
 
 function volumeDown() {
-    videoObj.volume-=0.1;
+    video.volume-=0.1;
 }
 
 function seek(sec) {
-    videoObj.currentTime+=sec;
+    video.currentTime+=sec;
 }
 
 playM3u8(window.location.href.split("#")[1])
