3d3c3753589e0865eb0e86ef3b75f66fc91e5386
Namito Yokota <namitoyokota@icloud.com>
2025-03-02 09:29:23 -0600

    Code clean up.

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # m3u8-player
+
 M3U8 Player in browser. Play any live stream m3u8 link with this webapp at https://bharadwajpro.github.io/m3u8-player
 
 I created this website because VLC is unstable in playing live streams. It buffers a lot.
@@ -6,14 +7,18 @@ I created this website because VLC is unstable in playing live streams. It buffe
 If you want to extract m3u8 files from live streams. You can use `youtube-dl -F <video-url>` to display all formats available
 and then type `youtube-dl -f <itag> -g <video-url>` for the direct link. Paste it in the website mentioned above and play it.
 If it doesn't work, then raise an issue.
-  
+
 If you want live streams legally then visit this repo - https://github.com/notanewbie/LegalStream
 
 This website is tested on Google Chrome and Firefox desktop web browsers. Not tested on android and iOS. Raise an issue if it doesn't work.
+
 ## Keyboard shortcuts
-* `up` and `down` - Volume
-* `left` and `right` - Seek by 5 secs
-* `f` - Full screen
+
+-   `up` and `down` - Volume
+-   `left` and `right` - Seek by 5 secs
+-   `f` - Full screen
+
 ## Known issues
-* The page is served over `https`. So if the stream url is not `https` but `http`, the browser may throw a warning. The content fails to load. Disable web security in Chrome to get around this.
-* CORS issue - If the media server blocks other IPs saying CORS. You might have to install this chrome extension to get around this - https://chrome.google.com/webstore/detail/allow-cors-access-control/lhobafahddgcelffkeicbaginigeejlf
+
+-   The page is served over `https`. So if the stream url is not `https` but `http`, the browser may throw a warning. The content fails to load. Disable web security in Chrome to get around this.
+-   CORS issue - If the media server blocks other IPs saying CORS. You might have to install this chrome extension to get around this - https://chrome.google.com/webstore/detail/allow-cors-access-control/lhobafahddgcelffkeicbaginigeejlf
--- player/player.js
+++ player/player.js
@@ -1,60 +1,61 @@
 var video = document.getElementById('video');
 
-function playM3u8(url){
-  if(Hls.isSupported()) {
-      video.volume = 0.3;
-      var hls = new Hls();
-      var m3u8Url = decodeURIComponent(url)
-      hls.loadSource(m3u8Url);
-      hls.attachMedia(video);
-      hls.on(Hls.Events.MANIFEST_PARSED,function() {
-        video.play();
-      });
-      document.title = url
+function playM3u8(url) {
+    if (Hls.isSupported()) {
+        video.volume = 0.3;
+        var hls = new Hls();
+        var m3u8Url = decodeURIComponent(url);
+        hls.loadSource(m3u8Url);
+        hls.attachMedia(video);
+        hls.on(Hls.Events.MANIFEST_PARSED, function () {
+            video.play();
+        });
+        document.title = url;
+    } else if (video.canPlayType('application/vnd.apple.mpegurl')) {
+        video.src = url;
+        video.addEventListener('canplay', function () {
+            video.play();
+        });
+        video.volume = 0.3;
+        document.title = url;
     }
-	else if (video.canPlayType('application/vnd.apple.mpegurl')) {
-		video.src = url;
-		video.addEventListener('canplay',function() {
-		  video.play();
-		});
-		video.volume = 0.3;
-		document.title = url;
-  	}
 }
 
 function playPause() {
-    video.paused?video.play():video.pause();
+    video.paused ? video.play() : video.pause();
 }
 
 function volumeUp() {
-    if(video.volume <= 0.9) video.volume+=0.1;
+    if (video.volume <= 0.9) video.volume += 0.1;
 }
 
 function volumeDown() {
-    if(video.volume >= 0.1) video.volume-=0.1;
+    if (video.volume >= 0.1) video.volume -= 0.1;
 }
 
 function seekRight() {
-    video.currentTime+=5;
+    video.currentTime += 5;
 }
 
 function seekLeft() {
-    video.currentTime-=5;
+    video.currentTime -= 5;
 }
 
 function vidFullscreen() {
     if (video.requestFullscreen) {
-      video.requestFullscreen();
-  } else if (video.mozRequestFullScreen) {
-      video.mozRequestFullScreen();
-  } else if (video.webkitRequestFullscreen) {
-      video.webkitRequestFullscreen();
+        video.requestFullscreen();
+    } else if (video.mozRequestFullScreen) {
+        video.mozRequestFullScreen();
+    } else if (video.webkitRequestFullscreen) {
+        video.webkitRequestFullscreen();
     }
 }
 
-playM3u8(window.location.href.split("#")[1])
+playM3u8(window.location.href.split('#')[1]);
 $(window).on('load', function () {
-    $('#video').on('click', function(){this.paused?this.play():this.pause();});
+    $('#video').on('click', function () {
+        this.paused ? this.play() : this.pause();
+    });
     Mousetrap.bind('space', playPause);
     Mousetrap.bind('up', volumeUp);
     Mousetrap.bind('down', volumeDown);
